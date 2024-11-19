ce28b8d9e216c91f7d7563c6e27ae7efa9113723
Heynnering <garavitoheynner@gmail.com>
2024-11-19 15:41:18 -0500

    Add files via upload

---
+++ templates/ltmypro/js/custom.js
@@ -0,0 +1,153 @@
+/*** Change Color Preset ***/
+jQuery(function ($) {  
+        a = $("a").css("color");
+        document.documentElement.style.setProperty('--background-color',a)
+        document.documentElement.style.setProperty('--text-color',a)
+});
+
+
+/*** Slider ***/
+jQuery(function ($) { 
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 355,
+      "density": {
+        "enable": true,
+        "value_area": 789.1476416322727
+      }
+    },
+    "color": {
+      "value": "#ffeb69"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.48927153781200905,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 0.2,
+        "opacity_min": 0,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 3,
+        "size_min": 0,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 0.2,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 83.91608391608392,
+        "size": 1,
+        "duration": 3,
+        "opacity": 1,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+});
+
+
+
+/*** Skill ***/
+jQuery(function ($) { 
+  $(".skills").addClass("active")
+  $(".skills .skill .skill-bar span").each(function() {
+     $(this).animate({
+        "width": $(this).parent().attr("data-bar") + "%"
+     }, 1000);
+     $(this).append('<b>' + $(this).parent().attr("data-bar") + '%</b>');
+  });
+  setTimeout(function() {
+     $(".skills .skill .skill-bar span b").animate({"opacity":"1"},1000);
+  }, 2000);
+});
+
+
+/*** Counter ***/
+jQuery(function ($) { 
+  $('.count').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 4000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+  });
+});
